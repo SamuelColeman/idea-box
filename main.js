aa4d2b7948d5afa1e1845a44eb78908186426bbe
Kate Malone <katerosemalone@gmail.com>
2019-07-18 10:24:34 -0600

    commented out persisted card work

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+Idea box 
+
 var globalArr = JSON.parse(localStorage.getItem('ideaArr')) || [];
 
 var ideaTitleInput = document.querySelector('.idea_title-input');
@@ -8,12 +10,59 @@ var ideaContainer = document.querySelector('.idea');
 
 ideaContainer.addEventListener('click', targetSaveBtn);
 
+
+
 function targetSaveBtn(e) {
 	e.preventDefault();
 	if (e.target.classList.contains('idea_save-btn')) {
 		makeNewIdea();
 	}
 }
+
+// persistedIdeas()
+
+// function persistedIdeas() {
+// 	console.log('persistedIdeas');
+// 	  globalArr.map(function(ideaObj) {
+//       var globalArr = new Idea (ideaObj.id, ideaObj.title, ideaObj.body, ideaObj.quality,
+//         ideaObj.star)
+//     }
+//   }
+//   console.log(globalArr);
+
+// function reappendCard() {
+// 	console.log('reappendCard')
+
+
+// function persist() {
+//   ideaArray.map(anIdea => appendCard(anIdea));
+// }
+
+// function findIndex(event) {
+//   var id = findID(event);
+//   for (var i = 0; i < ideaArray.length; i++) {
+//     if (id === ideaArray[i].id) {
+//       return parseInt(i);
+//     }
+//   }
+// }
+
+// function selectID(e) {
+//   return parseInt(e.target.closest('.box').dataset.id);
+// }
+//   function clearInput(input) {
+//   input.value = '';
+// }
+
+// function persistedIdeas(e) {
+	//reinstantiate the object with idea class
+	//i is less then length so it pulls out the first 
+	// looped though gloabal arr pulls out qualities seperately id title star body quality then put into function that instantiates a new class of idea then push everthing into a global array then use splice to delete the first half of it. get rid of non class. length array/2 then run funchtion that create new cards get info back into first function to create a card 
+	// run everything through a create card again to create a new card on the page. 
+	// map through array 
+// 	if (e.target)
+
+// }
 
 function makeNewIdea() {
 	var idea = new Idea( {
