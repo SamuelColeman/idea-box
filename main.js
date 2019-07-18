6d8e1f4615bcf7d2aadfb918769952af878dea84
Vanessa Randall <vanessalrandall@gmail.com>
2019-07-18 10:16:23 -0600

    Make ideas editable and begin work on starring ideas

--- main.js
+++ main.js
@@ -29,7 +29,10 @@ function makeNewIdea() {
 }
 var box = document.querySelector('.box');
 
-box.addEventListener('click', deleteCard);
+box.addEventListener('click', function() {
+	deleteCard()
+	// favoriteIdea()
+	});
 
 function deleteCard(e) {
 	if (e.target.classList.contains('img_btn-exit')) {
@@ -41,27 +44,36 @@ function appendNewCard(idea) {
 	box.insertAdjacentHTML('afterbegin',
 				`<section class="box_card" data-id=${idea.id}>
 			<header class="box_card-header">
-				<!-- images that are buttons -->
 				<input class="img_btn-star box_card-icon" src="images/star.svg" type="image">
+				<input class="img_btn-starActive box_card-icon hidden" src="images/star-active.svg" type="image">
 				<input class="img_btn-exit box_card-icon" src="images/delete.svg" type="image">
 			</header>
 			<section class="box_card-main">
-				<p class="box_card-title">${idea.title}</p>
-				<p class="box_card-body">${idea.body}</p>
+				<p class="box_card-title" contenteditable="true">${idea.title}</p>
+				<p class="box_card-body" contenteditable="true">${idea.body}</p>
 			</section>
 			<footer class="box_card-footer">
-				<!-- image button -->
 				<input class="footer_quality-up box_card-icon" src="images/upvote.svg" type="image">
 				<p class="box_card-quality">Quality:
 					<span class="box_card-value">Swill
 					</span>
 				</p>
-				<!-- image button -->
 				<input class="footer_quality-down box_card-icon" src="images/downvote.svg" type="image">
 			</footer>
 		</section>`);
 }
 
+// function favoriteIdea(e) {
+// 	var favoritedIdea = document.querySelector('.img_btn-starActive');
+// 	var inactiveStar = document.querySelector('.img_btn-star');
+// 	if (e.target.classList.contains('img_btn-star')) {
+// 		favoritedIdea.classList.remove('hidden');
+// 		inactiveStar.classList.add('hidden');
+// 	} else {
+// 		favoritedIdea.classList.add('hidden');
+// 		inactiveStar.classList.remove('hidden');
+// 	}
+// }
 
 // ideaArr[0].id 
 
